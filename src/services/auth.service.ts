5504f7e8dedb80750abab8a7154e2044011812d0
Amar Al Fatah <amaralfatah.me@gmail.com>
2025-03-27 16:00:58 +0700

    feat: implement login api

---
+++ src/services/auth.service.ts
@@ -0,0 +1,15 @@
+import {apiSlice} from "@/services/base-query";
+
+export const authApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (credentials) => ({
+                url: "/login",
+                method: "POST",
+                body: credentials,
+            })
+        })
+    })
+})
+
+export const {useLoginMutation} = authApi;
