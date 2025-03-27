d2b45bc49bd9f593d5120ef09d3d7c364131ab20
Amar Al Fatah <amaralfatah.me@gmail.com>
2025-03-27 15:47:08 +0700

    feat: setup redux rtk

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
+import type {Metadata} from 'next';
+import {Poppins} from 'next/font/google';
 import '@/app/globals.css';
 import Header from '@/components/molecules/header';
 import Footer from '@/components/molecules/footer';
-import { Toaster } from '@/components/atomics/toaster';
+import {Toaster} from '@/components/atomics/toaster';
+import ReduxProvider from "@/providers/redux";
 
 const poppins = Poppins({
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -19,25 +20,28 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-    children,
-}: Readonly<{
+                                       children,
+                                   }: Readonly<{
     children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>
-        <div className="lg:block hidden">
-          <Header />
-          {children}
-          <Footer />
-        </div>
-        <div className="block lg:hidden">
-          <div className="text-secondary h-screen text-2xl text-center leading-snug font-medium my-auto grid place-content-center">
-            Sorry, this page only supported on 1024px screen or above
-          </div>
-        </div>
-        <Toaster />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={poppins.className}>
+        <ReduxProvider>
+            <div className="lg:block hidden">
+                <Header/>
+                {children}
+                <Footer/>
+            </div>
+            <div className="block lg:hidden">
+                <div
+                    className="text-secondary h-screen text-2xl text-center leading-snug font-medium my-auto grid place-content-center">
+                    Sorry, this page only supported on 1024px screen or above
+                </div>
+            </div>
+            <Toaster/>
+        </ReduxProvider>
+        </body>
+        </html>
+    );
 }
