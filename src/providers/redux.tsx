acda47a072bf2edeeca96d507b7699ee4bfa3d5f
Amar Al Fatah <amaralfatah.me@gmail.com>
2025-04-02 22:10:57 +0700

    feat: implement next-auth login

--- src/providers/redux.tsx
+++ src/providers/redux.tsx
@@ -1,12 +1,17 @@
 // redux.ts
-"use client";
+'use client';
 
-import React from "react";
-import {Provider} from "react-redux";
-import {store} from "@/lib/store";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '@/lib/store';
+import { SessionProvider } from 'next-auth/react';
 
-function ReduxProvider({children}: { children: React.ReactNode }) {
-    return <Provider store={store}>{children}</Provider>
+function ReduxProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <Provider store={store}>{children}</Provider>
+    </SessionProvider>
+  );
 }
 
 export default ReduxProvider;
